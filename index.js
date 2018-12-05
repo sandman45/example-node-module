7ab06ee3d7400571490ec0aaef6de585ccf32978
sandman45 <msanders45@gmail.com>
2018-12-04 17:16:46 -0700

    example stuff

--- index.js
+++ index.js
@@ -4,11 +4,3 @@ module.exports = {
     draw: util.draw,
     draw2: util.draw2,
 };
-
-Promise.all([
-    module.exports.draw2('small'),
-    module.exports.draw2('medium'),
-    module.exports.draw2('large'),
-]).then((results)=>{
-   // console.log(results);
-});
